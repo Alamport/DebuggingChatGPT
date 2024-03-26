ed93afc1c3b3ae14052bd6a0461cc525b0e34d27
AdinLamport <108032137+Alamport@users.noreply.github.com>
2024-03-25 23:28:32 -0400

    Creation of Syntax Errors

---
+++ syntax_error/grammar_py.py
@@ -0,0 +1,14 @@
+def main():
+        # prices of fruits
+        apple = 2
+        banana = 3
+        milk = 4
+        
+        # error 1
+        total_cost = apple + banan + milk
+        
+        print(f"total cost of groceries is ${total_cost}")
+
+        # error 2
+        print(Groceries are done!)
+main()
