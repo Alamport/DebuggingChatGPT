9e50e0354d992c6e73ba83700dca8808b6626bd8
AdinLamport <108032137+Alamport@users.noreply.github.com>
2024-03-27 12:30:18 -0400

    Simple changes to grammar and syntax

--- syntax_error/syntax.c
+++ syntax_error/syntax.c
@@ -1,19 +1,21 @@
+#include <stdio.h>
+
 int main(int argc, char argv[])
 {
         int applePrice = 2;
         int bananaPrice = 3;
         int milkPrice = 4;
 
-        // error 1 fixed
-        if (applePrice == bananaPrice) {
+        // error 1
+        if (applePrice = bananaPrice) {
                 printf("Apples have the same price as bananas\n");
         }
 
-        // error 2 fixed
-        int totalCost = applePrice + bananaPrice + milkPrice;
+        // error 2
+        int totalCost = applePrice + bananPrice + milkPrice;
 
         printf("total cost of groceries is $%i\n", totalCost);
 
-        // error 3 fixed
-        printf("Groceries are done!");
+        // error 3
+        printf(Groceries are done!);
 }
