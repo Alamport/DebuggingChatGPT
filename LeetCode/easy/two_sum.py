5797ed0d0834cd6d556931ae054d2eca44794c58
AdinLamport <108032137+Alamport@users.noreply.github.com>
2024-03-27 12:30:03 -0400

    Created LeetCode Prompts

---
+++ LeetCode/easy/two_sum.py
@@ -0,0 +1,20 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_indices = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_indices:
+                return [num_indices[complement], i]
+            
+            # error --> the key and value should be switched
+            num_indices[i] = num
+
+        return []
---
+++ LeetCode/hard/reg_exp_matchmaking_C.py
@@ -0,0 +1,58 @@
+"""
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+"""
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        i, j = len(s) - 1, len(p) - 1
+        return self.backtrack({}, s, p, i, j)
+
+    def backtrack(self, cache, s, p, i, j):
+        key = (i, j)
+        if key in cache:
+            return cache[key]
+
+        if i == -1 and j == -1:
+            cache[key] = True
+            return True
+
+        if i != -1 and j == -1:
+            cache[key] = False
+            return cache[key]
+
+        if i == -1 and p[j] == '*':
+            k = j
+            while k != -1 and p[k] == '*':
+                k -= 2
+            
+            if k == -1:
+                cache[key] = True
+                return cache[key]
+            
+            cache[key] = False
+            return cache[key]
+        
+        if i == -1 and p[j] != '*':
+            cache[key] = False
+            return cache[key]
+
+        if p[j] == '*':
+            if self.backtrack(cache, s, p, i, j - 2):
+                cache[key] = True
+                return cache[key]
+            
+            if p[j - 1] == s[i] or p[j - 1] == '.':
+                if self.backtrack(cache, s, p, i - 1, j):
+                    cache[key] = True
+                    return cache[key]
+        
+        if p[j] == '.' or s[i] == p[j]:
+            if self.backtrack(cache, s, p, i - 1, j - 1):
+                cache[key] = True
+                return cache[key]
+
+        cache[key] = False
+        return cache[key]
